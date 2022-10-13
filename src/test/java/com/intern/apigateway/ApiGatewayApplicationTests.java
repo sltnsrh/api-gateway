e34a89e543fe0a6b28ff8c5b57da19506982ac89
sltnsrh <lifenjoy8888@gmail.com>
2022-10-13 12:43:20 +0300

    initial commit

---
+++ src/test/java/com/intern/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
